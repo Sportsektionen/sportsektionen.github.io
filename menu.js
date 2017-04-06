ded61413b702bac7b7f0db4f11900cc3007da826
Anton Österberg <anton.osterberg@gmail.com>
2017-04-06 17:14:11 +0200

    Have enabled dynamic loading of menu through java script, which enables us to load the same menu through each page with one line of java script.

---
+++ menu.js
@@ -0,0 +1,9 @@
+document.write("<div id='menu'>");
+document.write("<ul>");
+document.write("<li><a href='board'>Styrelsen</a></li>");
+document.write("<li><a href='activities'>Våra aktiviteter</a></li>");
+document.write("<li><a href='getInvolved'>Gå med i Sportsektionen</a></li>");
+document.write("<li><a href='skiweek'>Åre Skiweek</a></li>");
+document.write("<li><a href='hallOfFame'>Hall of Fame</a></li>");
+document.write("</ul>");
+document.write("</div>");
